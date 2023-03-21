c301dc8a5a78c61ee2a62dde232585e9c2f292ea
Ssmitika <sridevirs87@gmail.com>
2023-03-21 22:16:11 +0530

    completed

---
+++ If.java
@@ -0,0 +1,25 @@
+package looping;
+
+import java.util.Scanner;
+
+public class If {
+	
+	void m7()
+	{
+	     Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the age");
+		int age=sc.nextInt();
+		if(age>=18)
+		System.out.println("You are eligible to vote");
+		else
+	    System.out.println("You are not eligible to vote");		
+			
+	}
+	public static void main(String[] args)
+	{
+		If  obj1=new If();
+		obj1.m7();
+	}	
+
+}
